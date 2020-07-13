0a24502801b79f1e53255a90a0681a9b291391ed
Vassilyev Denis <tarapiygin@gmail.com>
2020-07-13 13:58:15 +0300

    function parameters changed

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -2,11 +2,12 @@ import healthIndicator from '../app';
 
 test.each([
   ['healthy', { name: 'Маг', health: 100 }, 'healthy'],
-  ['wounded', { name: 'Маг', health: 99.999 }, 'wounded'],
-  ['wounded', { name: 'Маг', health: 30 }, 'wounded'],
-  ['wounded', { name: 'Маг', health: 55 }, 'wounded'],
-  ['critical', { name: 'Маг', health: 29.999 }, 'critical'],
-  ['critical', { name: 'Маг', health: 15 }, 'critical'],
+  ['healthy', { name: 'Маг', health: 51 }, 'healthy'],
+  ['wounded', { name: 'Маг', health: 50 }, 'wounded'],
+  ['wounded', { name: 'Маг', health: 15 }, 'wounded'],
+  ['wounded', { name: 'Маг', health: 33 }, 'wounded'],
+  ['critical', { name: 'Маг', health: 14 }, 'critical'],
+  ['critical', { name: 'Маг', health: 3 }, 'critical'],
   ['critical', { name: 'Маг', health: 0 }, 'critical'],
 ])(
   ('must show the level of health - %s'),
@@ -15,15 +16,3 @@ test.each([
     expect(result).toBe(expected);
   },
 );
-
-// test('should show "wounded" health level in healthIndicator', () => {
-//   const received = healthIndicator({ name: 'Маг', health: 31 });
-//   const expected = 'wounded';
-//   expect(received).toBe(expected);
-// });
-
-// test('should show "critical" health level in healthIndicator', () => {
-//   const received = healthIndicator({ name: 'Маг', health: 29 });
-//   const expected = 'critical';
-//   expect(received).toBe(expected);
-// });
--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 export default function healthIndicator(params) {
   let result = '';
-  if (params.health >= 100) {
+  if (params.health > 50) {
     result = 'healthy';
-  } else if (params.health >= 30) {
+  } else if (params.health >= 15) {
     result = 'wounded';
   } else {
     result = 'critical';
